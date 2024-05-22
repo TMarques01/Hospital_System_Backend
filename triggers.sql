f38403251582d09a11715f29a69b29983a073ab1
RuiOliv4444 <132606623+RuiOliv4444@users.noreply.github.com>
2024-05-22 16:55:44 +0100

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -36,3 +36,25 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION create_hospitalization_on_surgery() RETURNS TRIGGER AS $$
+DECLARE
+    new_hosp_id INTEGER;
+BEGIN
+    INSERT INTO hospitalization (date_start, date_end, n_bed, assistants_contract_employee_person_id, pacient_person_id, nurse_contract_employee_person_id)
+    VALUES (NEW.date_surgery, NEW.date_surgery + INTERVAL '1 day', (SELECT COALESCE(MAX(n_bed), 0) + 1 FROM hospitalization), NEW.assistant_id, NEW.pacient_id, NEW.nurse_id)
+    RETURNING id INTO new_hosp_id;
+    
+    NEW.hospitalization_id := new_hosp_id;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS surgery_before_insert ON surgery;
+
+CREATE TRIGGER surgery_before_insert
+BEFORE INSERT ON surgery
+FOR EACH ROW
+EXECUTE FUNCTION create_hospitalization_on_surgery();
