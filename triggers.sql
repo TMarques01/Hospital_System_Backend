72eee3eb3625698fee2982204f90287427f0bda1
RuiOliv4444 <132606623+RuiOliv4444@users.noreply.github.com>
2024-05-22 16:46:05 +0100

    trigger da hospitalização

--- triggers.sql
+++ triggers.sql
@@ -1,42 +1,38 @@
 INSERT INTO billing 
 (id, total, data_billing) 
-VALUES (0, 0, '1111-11-11')0
+VALUES (0, 0, '1111-11-11')
 
--- Appointment billing trigger
 
-CREATE OR REPLACE FUNCTION create_billing_on_appointment()
-RETURNS TRIGGER AS $$
-DECLARE
-    new_billing_id INTEGER;
+
+CREATE OR REPLACE FUNCTION create_billing_on_appointment() RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO billing (id, total, data_billing)
-    VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM billing), 50, CURRENT_DATE)
-    RETURNING id INTO new_billing_id;
-
-    UPDATE appointment SET billing_id = new_billing_id WHERE id = NEW.id;
-
+    VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM billing), 50, NOW());
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS appointment_after_insert ON appointment;
+DROP TRIGGER IF EXISTS appointment_before_insert ON appointment;
 
-CREATE TRIGGER appointment_after_insert
-AFTER INSERT ON appointment
+CREATE TRIGGER appointment_before_insert
+BEFORE INSERT ON appointment
 FOR EACH ROW
 EXECUTE FUNCTION create_billing_on_appointment();
 
--- Easy to visualize the data
 
-SELECT person.id, person.username, 
-CASE 
-	WHEN assistants.contract_employee_person_id IS NOT NULL THEN 'assistant'
-	WHEN doctor.contract_employee_person_id IS NOT NULL THEN 'doctor'
-	WHEN nurse.contract_employee_person_id IS NOT NULL THEN 'nurse'	
-	ELSE 'pacient'
-END as specification
-FROM person
-LEFT JOIN assistants ON person.id = assistants.contract_employee_person_id
-LEFT JOIN doctor ON person.id = doctor.contract_employee_person_id
-LEFT JOIN nurse ON person.id = nurse.contract_employee_person_id
-ORDER BY id;
+
+
+
+CREATE OR REPLACE FUNCTION create_billing_on_hospitalization() RETURNS TRIGGER AS $$
+DECLARE
+    new_billing_id INTEGER;
+BEGIN
+    INSERT INTO billing (id, total, data_billing)
+    VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM billing), 100, NOW())
+    RETURNING id INTO new_billing_id;
+    
+    NEW.billing_id := new_billing_id;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
